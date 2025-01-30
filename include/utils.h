e11fd6510dca9aa47bb142be224c36511425b7f7
Valeria <alu0101485287@ull.edu.es>
2025-01-30 12:49:37 +0000

    ADDED: Text Format Defines

--- include/utils.h
+++ include/utils.h
@@ -9,6 +9,14 @@
   #define CLEAR_SCREEN "clear"
 #endif
 
+#define RESET       "\033[0m"
+#define BOLD        "\033[1m"
+
+#define RED         "\033[31m"
+#define GREEN       "\033[32m"
+#define YELLOW      "\033[33m"
+#define BLUE        "\033[34m"
+
 void ShowMenu();
 void ShowHelp();
 void ProcessEncryption();
